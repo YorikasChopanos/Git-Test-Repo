018b9bc80f8490247af07930b9a21110552b768f
Yorikas <yorikaskg@gmail.com>
2022-03-26 16:05:43 +0200

    Divisor functionality error, floating point exception

--- c_tutorial.c
+++ c_tutorial.c
@@ -35,33 +35,25 @@ int getStableTerm() {
   return stableTerm;
 };
 
-int findDivisors(int stableTerm, int i, int j) {
-  if (stableTerm % j == 0) {
-    return j;
-  } else {
-    return 1;
-  };
-};
 
 int main() {
 
-  int stableTerm;
-  int i;
   int polynomialDegree = getPolynomialDegree();
   int coefficients[polynomialDegree];
-  for (i = 0; i < polynomialDegree; i++) {
+  for (int i = 0; i < polynomialDegree; i++) {
     coefficients[i] = getCoefficients();
   };
 
-  stableTerm = getStableTerm();
+  int stableTerm = getStableTerm();
   int divisor[stableTerm];
-  int j = 1;
-  for (i = 0; i < stableTerm; i++) {
-    divisor[i] = findDivisors(stableTerm, i, j);
-    divisor[i + 1] = divisor[i] * -1;
-    i++;
-    j++;
-  };
+  int divisorCounter = 1;
+  for (int i = 0; i <= stableTerm; i=+2) {
+     if (stableTerm % divisorCounter == 0) {
+         divisor[i] = divisorCounter;
+         printf("%d", divisor[i]);
+     };
+     divisorCounter++;
+   };
 
   //     int remainderIsZero = 0;
   //     int newCoefficient[polynomialDegree];
